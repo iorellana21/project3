ea55b69c80e8a6b625a2e51790da97dbc536a9fd
malhill <malcolmhc@msn.com>
2021-05-23 16:14:12 -0400

    added nav tabs to dashboard

--- client/src/components/Friendlist/index.js
+++ client/src/components/Friendlist/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import './index.scss';
+import React from "react";
+import { Tab, Nav, Button, Modal } from "react-bootstrap";
+import { FaSearch } from "react-icons/fa";
+import "./index.scss";
 
-
+const CONVERSATIONS_KEY = "conversations";
+const FRIENDS_KEY = "friends";
 
 export default function Friendlist({ list }) {
-    console.log(list);
+  console.log(list);
 
-    return (
-        <div className="align-items-center friend-list ">
-            <ul>
-                {list.map((friend) => (<li key={friend.userName}>{friend.userName}</li>))}
-            </ul>
-        </div>
-    )
-};
+//   const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY);
+//     const conversationsOpen = activeKey === CONVERSATIONS_KEY;
+//     const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <div>
+      <Nav variant="tabs" className="justify-content-center">
+        <Nav.Item>
+          <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey={FRIENDS_KEY}>Friends</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      {/* <Modal show={modalOpen} onHide={closeModal}>
+        {conversationsOpen ? (
+          <NewConversationModal close={closeModal} />
+        ) : (
+          <NewFriendModal close={closeModal} />
+        )}
+      </Modal> */}
+
+    </div>
+  );
+}
